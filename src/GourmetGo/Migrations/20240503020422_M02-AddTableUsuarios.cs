024f562d18df4531cedd6d22d48d996659a79594
leandroborth <leandroborth@gmail.com>
2024-05-09 10:39:35 -0300

    Adicionando projeto ASP.NET Core MVC

---
+++ src/GourmetGo/Migrations/20240503020422_M02-AddTableUsuarios.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GourmetGo.Migrations
+{
+    /// <inheritdoc />
+    public partial class M02AddTableUsuarios : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<double>(
+                name: "Preco",
+                table: "Produtos",
+                type: "float",
+                nullable: false,
+                oldClrType: typeof(float),
+                oldType: "real");
+
+            migrationBuilder.CreateTable(
+                name: "Usuarios",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Cpf = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Telefone = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Senha = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Tipo = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Usuarios", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Usuarios");
+
+            migrationBuilder.AlterColumn<float>(
+                name: "Preco",
+                table: "Produtos",
+                type: "real",
+                nullable: false,
+                oldClrType: typeof(double),
+                oldType: "float");
+        }
+    }
+}
---
+++ src/GourmetGo/Models/Produto.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GourmetGo.Models
+{
+    [Table("Produtos")]
+    public class Produto
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório informar o nome!" )]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório informar a descrição!")]
+        [Display(Name = "Descrição")]
+        public string Descricao { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório informar o preço!")]
+        [Display(Name = "Preço")]
+        public double Preco { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório informar a categoria!")]
+        public Categoria Categoria { get; set; }
+    }
+
+    public enum Categoria
+    {
+        Comida,
+        Bebida
+    }
+}
