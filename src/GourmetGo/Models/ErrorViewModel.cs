bd26d3af2e514224f21d18f9776377236492f219
luiz-hovadich <luizhovadich@gmail.com>
2024-05-12 22:26:57 -0300

    SRC 2

--- src/GourmetGo/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace GourmetGo.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
