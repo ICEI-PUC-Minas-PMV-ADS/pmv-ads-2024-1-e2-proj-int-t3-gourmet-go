9253740f252066d33d9b949009bcb7f99cc202e8
Marina <146393136+marinadionysio1@users.noreply.github.com>
2024-05-12 21:29:56 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace Estoque
+{
+    public class Program
+    {
+        public static (string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+            builder.services.addRazorPages()AddRazorCompilation ();
+
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
