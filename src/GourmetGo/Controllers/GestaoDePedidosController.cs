b336252d1652d1ac0cbb0802fa756a11d9c1222e
Lucas Xavier <lucasbebianolbx@gmail.com>
2024-05-29 18:30:12 -0300

    Removi as funçoes que nao seram usadas no Controller e na View.

--- src/GourmetGo/Controllers/GestaoDePedidosController.cs
+++ src/GourmetGo/Controllers/GestaoDePedidosController.cs
@@ -45,31 +45,6 @@ namespace GourmetGo.Controllers
             return View(pedido);
         }
 
-        // GET: GestaoDePedidos/Create
-        public IActionResult Create()
-        {
-            ViewData["ProdutoId"] = new SelectList(_context.Produtos, "Id", "Descricao");
-            ViewData["UsuarioId"] = new SelectList(_context.Usuarios, "Id", "Cpf");
-            return View();
-        }
-
-        // POST: GestaoDePedidos/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,UsuarioId,ProdutoId,Observações,Tipo,Endereço,Pagamento,Status")] Pedido pedido)
-        {
-            if (ModelState.IsValid)
-            {
-                _context.Add(pedido);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
-            ViewData["ProdutoId"] = new SelectList(_context.Produtos, "Id", "Descricao", pedido.ProdutoId);
-            ViewData["UsuarioId"] = new SelectList(_context.Usuarios, "Id", "Cpf", pedido.UsuarioId);
-            return View(pedido);
-        }
 
         // GET: GestaoDePedidos/Edit/5
         public async Task<IActionResult> Edit(int? id)
@@ -124,41 +99,6 @@ namespace GourmetGo.Controllers
             ViewData["ProdutoId"] = new SelectList(_context.Produtos, "Id", "Descricao", pedido.ProdutoId);
             ViewData["UsuarioId"] = new SelectList(_context.Usuarios, "Id", "Cpf", pedido.UsuarioId);
             return View(pedido);
-        }
-
-        // GET: GestaoDePedidos/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var pedido = await _context.Pedidos
-                .Include(p => p.Produto)
-                .Include(p => p.Usuario)
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (pedido == null)
-            {
-                return NotFound();
-            }
-
-            return View(pedido);
-        }
-
-        // POST: GestaoDePedidos/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            var pedido = await _context.Pedidos.FindAsync(id);
-            if (pedido != null)
-            {
-                _context.Pedidos.Remove(pedido);
-            }
-
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
         }
 
         private bool PedidoExists(int id)
