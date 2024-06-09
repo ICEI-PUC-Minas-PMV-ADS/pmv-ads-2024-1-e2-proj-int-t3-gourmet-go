4cff587a53fec858bdf28cac9764f34218d501cf
Lucas Xavier <lucasbebianolbx@gmail.com>
2024-06-08 22:32:47 -0300

    Redirecionamento ao Cadastrar

--- src/GourmetGo/Controllers/UsuariosController.cs
+++ src/GourmetGo/Controllers/UsuariosController.cs
@@ -168,7 +168,7 @@ namespace GourmetGo.Controllers
                 usuario.Senha = BCrypt.Net.BCrypt.HashPassword(usuario.Senha);
                 _context.Add(usuario);
                 await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
+                return RedirectToAction("Login", "Usuarios");
             }
             return View(usuario);
         }
